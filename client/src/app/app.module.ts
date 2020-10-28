053dbdfabdd118e0424d3b0a0b19424fc9200b1e
Mihai <runcan.mihai@gmail.com>
2020-10-28 23:11:51 +0200

    added pagination

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { ShopModule } from './shop/shop.module';
 
 @NgModule({
