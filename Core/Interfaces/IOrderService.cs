f9f2ad09d80b8d8899c3b631fed4bc5364853d2d
Mihai <runcan.mihai@gmail.com>
2020-11-29 16:43:35 +0200

    created API for orders

---
+++ Core/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.OrderAggregate;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketid,
+            Address shippingAddress);
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
+        Task<Order> GetOrderByIdAsync(int id, string buyerEmail);
+        Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+    }
+}
